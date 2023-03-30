f4c4f3689cbc69e6b612710437907f7bc3eca289
Tuco_1001 <arthursgrasso@gmail.com>
2023-03-29 21:49:59 -0300

    o

---
+++ listas.py
@@ -0,0 +1,65 @@
+
+
+lista = ["chicago", "queens", 'salvador', 'pernambuco']
+print(lista)
+
+lista2 = [2, 3, 7, 8, 10]
+print(lista2)
+
+lista3 = [2.0, 3.5, 6.3]
+print(lista3)
+
+lista2 = lista2 + lista3 
+
+lista4 = [True, False]
+print(lista4)
+
+# index     0       1       2     3    4  5   -> 6 elementos
+lista5 = [True, "chicago", 2.5, False, 4, 8]
+print(lista5)
+
+print(type(lista2))
+
+print(lista5[1])
+
+print(lista5[-6])
+
+# Slicing
+
+print(lista5[::])   # dois pontos representa começo e fim
+print(lista5[1:])   # retorna do index que destacamos até o fim da lista
+print(lista5[2:])   # retorna do index que destacamos até o fim da lista
+print(lista5[:3])   # retorna do começo da lista até o index -1
+print(lista5[:4])   # retorna do começo da lista até o index -1
+print(lista5[1:4])  # retorna do index destacado até o index -1
+print(lista5[1:6:2])
+
+nome5 = "terra"
+
+print(nome5[2:4])
+
+
+lista1 = [2.0, 3.5, 4.7]
+print(lista1)
+
+lista2 = [1, 5, 9, 11, 15]
+print(lista2)
+
+# index     0        1        2  
+lista3 = ['Nome', 'Nome2', 'Nome']
+
+print(len(lista3))
+
+# Tamanho da lista - função len
+print(len(lista1))
+print(len(lista2))
+
+# Funções que só podem ser utilizadas com tipos de dados numéricos
+
+print(sum(lista1))  # Somatório dos elementos da nossa lista
+x = 2.0 + 3.5 + 4.7
+print(x)
+
+print(max(lista2))  # Retorna o elemento de valor máximo da lista
+
+print(min(lista1))  # Retorna o elemento de valor mínimu da lista
